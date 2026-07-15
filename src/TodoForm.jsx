08fd6f61f32ef93025b57fbd23c9b7c1ff422005
Elmira.Y <e4016262@gmail.com>
2026-07-15 05:28:11 -0400

    lesson-02-components

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,14 +1,12 @@
 export default function TodoForm() {
 
     return (
-      <>
-        <form>
-          <label htmlFor="todoTitle">Todo</label>
-          <input id="todoTitle" type="text" />
-          <button type="submit" disabled>
-            Add Todo
-          </button>
-        </form>
-        </>
+      <form>
+        <label htmlFor="todoTitle">Todo</label>
+        <input id="todoTitle" type="text" />
+        <button type="submit" disabled>
+          Add Todo
+        </button>
+      </form>
     );
 }
