47975695c8b1d6cf787e7258fa123b49b462b06c
Elmira.Y <e4016262@gmail.com>
2026-07-14 21:04:13 -0400

    lesson-02-components

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -4,19 +4,21 @@ export default function TodoList() {
     { id: 2, title: "take notes" },
     { id: 3, title: "code out app" },
   ];
-  
+  /*
   const listColors = ["var(--list-color1)",  "var(--list-color2)",  "var(--list-color3)",  "var(--list-color4)"]
+  */
 
 
   return (
        <ul>
         {todoList.map((todo, index) => (
-          <li
+         /* <li
             className="todo-item"
             style={{ backgroundColor: listColors[index % listColors.length] }}
             key={todo.id}
           >
-            {todo.title}
+          */
+               <li className="todo-item" key={todo.id}> {todo.title}
           </li>
         ))}
       </ul>
