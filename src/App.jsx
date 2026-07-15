8aee3bb10314db1895c15451bc966ae1619e8f63
Elmira.Y <e4016262@gmail.com>
2026-07-14 21:51:08 -0400

    chancg lesson-01

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,14 @@ function App() {
 
   return (
     <div>
-    <h1>My Todos</h1> 
-     <ul>
-      {todoList.map(todo => <li key={todo.id}>{todo.title}</li>)}
-     </ul>
+      <h1>Todo List</h1>
+      <ul>
+        {todoList.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default App
