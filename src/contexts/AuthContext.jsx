2c77adeac6182549da19316b978ba8a9f81e7d8c
Elmira.Y <e4016262@gmail.com>
2026-09-12 13:46:11 -0400

    lesson-099

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -92,6 +92,6 @@ export function AuthProvider({ children }) {
   return (
      <AuthContext.Provider value={value}>
         {children}
-    </AuthContext.Provider>;
+    </AuthContext.Provider>
   )
 }
