594126e1e585d6398bcf1748d5c06c36650bf28b
Elmira.Y <e4016262@gmail.com>
2026-09-12 13:43:51 -0400

    lesson-099

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -7,7 +7,7 @@ export const AuthContext = createContext();
 export function useAuth() {
   const context = useContext(AuthContext);
   if (!context) {
-    throw new Error("useAuth must be used within an AuthProvider");
+    throw new Error("useAuth must be used within an AuthProvider")
   }
   return context;
 }
@@ -89,5 +89,9 @@ export function AuthProvider({ children }) {
     logout,
   };
 
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+  return (
+     <AuthContext.Provider value={value}>
+        {children}
+    </AuthContext.Provider>;
+  )
 }
